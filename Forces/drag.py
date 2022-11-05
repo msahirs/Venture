c85a07f73c75c099dea24947de67c841c271228a
ssujahudeen <sujahudeen.sahir@gmail.com>
2022-11-05 16:15:05 +0100

    updated force models

--- Forces/drag.py
+++ Forces/drag.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-def get_drag(rho, velocity, drag_coeff, ref_area):
+def calculate_drag(rho, velocity, drag_coeff, ref_area):
 
     q = 0.5 * rho * np.abs(velocity) * velocity
 
--- Forces/gravity.py
+++ Forces/gravity.py
@@ -1,6 +1,13 @@
 import numpy as np
 
-def constant_gravity(): # returns gravity as positive value in 
+def constant_gravity(): # returns constant gravity with positive axis pointing away from earth's centre
 
+    return np.array([[0,0,-9.80665]]).T
 
-    return 
+def newton_point(height=0):
+    mu_earth = 398600.436e9
+    r_earth = 6.371e6
+    g = -mu_earth/(r_earth + height)**2
+
+    return np.array([[0,0,g]]).T
+    
