d4de8bee14ee42752153f11bc5435ff7826f5d14
ssujahudeen <sujahudeen.sahir@gmail.com>
2022-11-26 20:18:59 +0100

    Added drag test function

--- Forces/drag.py
+++ Forces/drag.py
@@ -6,3 +6,16 @@ def calculate_drag(rho, velocity, drag_coeff, ref_area):
     q = 0.5 * rho * np.abs(velocity) * velocity
 
     return - q * drag_coeff * ref_area
+
+
+def calculate_drag_test(rho, velocity, drag_coeff, ref_area):
+    ans = calculate_drag(rho, velocity, drag_coeff, ref_area)
+
+    if np.sum(np.where(ans*velocity<0,1,0)) == 3:
+        print("True")
+
+    else:
+        raise ArithmeticError("Drag vector not oppposite to velocity vector")
+
+
+
